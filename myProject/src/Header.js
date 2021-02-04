70e10c57f8dbc7aaa148f60c1ad75afd4efe0c34
DrDersu <58595037+DrDersu@users.noreply.github.com>
2021-02-04 14:45:52 +0600

    Add files via upload

--- myProject/src/Header.js
+++ myProject/src/Header.js
@@ -1,17 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'jquery/dist/jquery.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
-function Header() {
+
+
+function Header(props){
     return (
-      <nav className="navbar navbar-dark bg-dark w-100 text-left">
-        <a className="navbar-brand offset-1" href="#">News <span className="ml-2 navbar-brand text-danger">PORTAL</span></a>
-        <div className="row w-75">
-          <a className="col-1 offset-1 text-light nav-link" href="#">Home</a>
-          <a className="col-1 offset-1 text-light nav-link" href="#">Contact</a>
-          <a className="col-2 offset-1 text-light nav-link" href="#">Archive</a>
-          <a className="col-2 offset-1 text-light nav-link" href="#">Log in</a>
-        </div>
-      </nav>
+        <nav className="navbar navbar-dark bg-success w-100 text-left">
+            <div className="container">
+                <a className="navbar-brand fw-bold" href="#">e-shopping</a>
+                <a className="text-light nav-link ms-auto" href="#">{props.state.onlineStatus?props.state.name:"Register"}</a>
+                <a className="text-light nav-link" href="#">RUS</a>
+                <a className="text-light nav-link" href="#">ENG</a>
+            </div>
+        </nav>
     )
-  }
-  export default Header;
+}
+
+export default Header;
--- myProject/src/Main.js
+++ myProject/src/Main.js
@@ -1,18 +1,103 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'jquery/dist/jquery.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
+import './Main.css'
 import Header from './Header';
-import Search from './Search';
-import Article from './Article';
+import Content from './Content';
+import Footer from './Footer';
+import React from "react";
+import Elements from "./Elements";
 
-function Main() {
-    return (
-        <div>
-            <Header />
-            <Search />
-            <Article />
-        </div>
-    );
+const users = [
+    {id: 1, email: 'admin@mail.ru', password: 'admin', username: 'Admin'},
+    {id: 2, email: 'erlan@mail.ru', password: 'erlan', username: 'Erlan'},
+    {id: 3, email: 'john@gmail.com', password: 'john123', username: 'John'},
+    {id: 4, email: 'alexia@yahoo.com', password: 'qwertyalex', username: 'Alexia'}
+]
+
+class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {onlineStatus: false, name: 'null'};
+        this.loginUser = this.loginUser.bind(this);
+        this.logoutUser = this.logoutUser.bind(this);
+    }
+
+
+    loginUser() {
+        var email_input = document.getElementById('email');
+        var pass_input = document.getElementById('password');
+        var a = true;
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].email===email_input.value){
+                a = false;
+                if (users[i].password===pass_input.value){
+                    this.setState({onlineStatus: true, name: users[i].username});
+                }
+                else{
+                    document.getElementById('alert2').className = 'text-danger';
+                    pass_input.style.borderBottomColor = 'red';
+                }
+            }
+        }
+        if(a){
+            document.getElementById('alert1').className = 'text-danger';
+            email_input.style.borderBottomColor = 'red';
+        }
+    }
+    logoutUser(){
+        this.setState({onlineStatus: false});
+    }
+
+    render() {
+        if(this.state.onlineStatus){
+            return(
+                <div>
+                    <Header state={this.state}/>
+                    <div className="container content">
+                        <div className="row mt-4">
+                            <div className="col-3">
+                                <h6>Username: <span className='text-primary'>{this.state.name}</span></h6>
+                                <p className='text-muted mt-3'>status: <span className='text-success'>Online</span></p>
+                                <button className='btn btn-danger mt-5' onClick={this.logoutUser}>Log out</button>
+                            </div>
+                            <div className="col-9 p-0">
+                                <Elements/>
+                            </div>
+                        </div>
+                    </div>
+                    <Footer/>
+                </div>
+            )
+        }else {
+            return (
+                <div>
+                    <Header state={this.state}/>
+                    <div className="container content">
+                        <div className="row mt-4">
+                            <div className="col-3">
+                                <label className="text-muted">Email</label>
+                                <input type="email" id="email" className='signInput'/>
+                                <p className='d-none' id='alert1'>Email is incorrect or not existed</p>
+                                <label className="text-muted mt-3">Password</label>
+                                <input type="password" id="password" className="signInput"/>
+                                <p className='d-none' id='alert2'>Password is incorrect</p>
+                                <button className='btn btn-success mt-3' onClick={this.loginUser}>SIGN IN ></button>
+                                <div className='w-100 mt-3'>
+                                    <input type="checkbox" name='remember' className='form-check-input'/>
+                                    <label className='form-check-label ms-3 text-muted'>Remember me</label>
+                                </div>
+                            </div>
+                            <div className="col-9 p-0">
+                                <Elements/>
+                            </div>
+                        </div>
+                    </div>
+                    <Footer/>
+                </div>
+            )
+        }
+    }
 }
 
 export default Main;
