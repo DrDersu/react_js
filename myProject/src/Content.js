46216657b85ae5e24a03c894a70e7616706e2c96
DrDersu <58595037+DrDersu@users.noreply.github.com>
2021-04-13 04:46:34 +0300

    Delete myProject directory

--- myProject/src/Content.js
+++
@@ -1,7 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'jquery/dist/jquery.min.js'
-import 'bootstrap/dist/js/bootstrap.min.js'
-function Content(){
-    return 'Content';
-}
-export default Content;
